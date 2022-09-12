fc8b25b4d1b844e42b6537a711339361bcec115f
Diovanna Schell <diovannacschell@gmail.com>
2022-09-12 18:08:02 -0300

    ajustes para lançar versão beta do app

---
+++ app/src/main/java/com/aplicativo/diseasedetector/GreeningActivity.java
@@ -0,0 +1,14 @@
+package com.aplicativo.diseasedetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GreeningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_greening);
+    }
+}
---
+++ app/src/main/java/com/aplicativo/diseasedetector/HealtyActivity.java
@@ -0,0 +1,14 @@
+package com.aplicativo.diseasedetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HealtyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_healty);
+    }
+}
